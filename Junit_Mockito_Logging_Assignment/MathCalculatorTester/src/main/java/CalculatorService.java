694e2ffacaf4a7c5d63407e21ff7609342426dc1
Shubham191296 <77062854+Shubham191296@users.noreply.github.com>
2021-01-27 16:39:58 +0530

    Delete Junit_Mockito_Logging_Assignment/MathCalculatorTester directory

--- Junit_Mockito_Logging_Assignment/MathCalculatorTester/src/main/java/CalculatorService.java
+++
@@ -1,9 +0,0 @@
-
-public interface CalculatorService {
-
-    public double add(double number1, double number2);
-    public double subtract(double number1, double number2);
-    public double multiply(double number1, double number2);
-    public double divide(double number1, double number2) throws Exception;
-
-}
